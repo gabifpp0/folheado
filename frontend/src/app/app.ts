b75ad9b23ea42ce49d8e92ec77be3dec0667e2bf
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2025-11-12 14:45:39 +0000

    chore: corrige nome do projeto do frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-folheadoB');
+  protected readonly title = signal('frontend');
 }
